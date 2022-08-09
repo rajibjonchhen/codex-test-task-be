3766bdc5b2bc71f6655ba8c7a4c668dfc47752bd
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-08-09 12:44:21 +0100

    users route initiated and check credential of user for login added

--- src/services/users/user-schema.js
+++ src/services/users/user-schema.js
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+
 
 const { Schema, model } = mongoose;
 
@@ -13,6 +15,34 @@ const userSchema = new Schema({
 },{
     timestamps: true,
 })
+
+
+userSchema.pre("save", async function(next){
+    const newUser = this
+    const plainPw = newUser.password
+    if(newUser.isModified("password")){
+        const hash = await bcrypt.hash(plainPw,11)
+    }
+    next()
+})
+
+
+UserSchema.methods.toJSON = function() {
+    const userDocument = this
+    const userObject = userDocument.toObject()
+    delete userObject.password
+    return userObject
+}
+
+UserSchema.statics.checkCredentials = async function(email, plainPW){
+    const user = await this.findOne({email})
+    if(user){
+        const isMatch = bcrypt.compare(plainPW, user.password)
+        return isMatch? user: null
+         
+    } else
+        return null
+}
 
 export default model("Users", userSchema)
 
--- src/services/users/usersRouter.js
+++ src/services/users/usersRouter.js
@@ -1,4 +1,19 @@
 import express from "express";
 const usersRouter = express.Router()
 
+//**************** post user *********************/
+usersRouter.post("/", (req, res, next) => {
+
+})
+
+//**************** get all users ******************/
+usersRouter.get("/", (req, res, next) => {
+
+})
+
+//**************** post user *********************/
+usersRouter.get("/:userId", (req, res, next) => {
+
+})
+
 export default usersRouter;
